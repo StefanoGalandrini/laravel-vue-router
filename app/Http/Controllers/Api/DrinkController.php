2c5f3f24d65b8610081a07df4f112a4c5bc58a98
StefanoGalandrini <stefe68@gmail.com>
2023-07-22 01:02:05 +0200

    Fix index method - query only on frontend

--- app/Http/Controllers/Api/DrinkController.php
+++ app/Http/Controllers/Api/DrinkController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Drink;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
 class DrinkController extends Controller
 {
 
     public function index(Request $request)
     {
-        $drinks = Drink::paginate(6);
+        $drinks = Drink::all();
 
         return response()->json([
             'success'   => true,
